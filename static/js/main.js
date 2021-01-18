4a9725f6042dca115f879ad5a2742a02d868c9ca
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2021-01-18 17:39:05 +0200

    add loader

--- static/js/main.js
+++ static/js/main.js
@@ -42,13 +42,21 @@ function loadValues(callback) {
 }
 
 // Show card with Results
-function showResults() {
+function Results() {
+  // Show loader
+  document.getElementById('loading').classList.remove('d-none');
   mainCalculation();
+  setTimeout(showResults, 500);
+}
+
+function showResults(){
+  document.getElementById('loading').classList.add('d-none');
   const resultsUI = document.getElementById("results");
   if (resultsUI.classList.contains("d-none")) {
     resultsUI.classList.remove("d-none");
   }
 }
+
 
 // hide card with Results
 function hideResults(){
